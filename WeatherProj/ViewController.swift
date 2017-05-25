f9486994f8c8390ccd68199c76da6ae36cb635fd
BankierJulien <julienbankiernyc@gmail.com>
2017-05-25 15:06:04 -0400

    Initial Commit

---
+++ WeatherProj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherProj
+//
+//  Created by Julien Bankier on 5/25/17.
+//  Copyright © 2017 Julien Bankier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
